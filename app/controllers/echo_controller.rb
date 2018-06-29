a7701d63fd9a2c928f3f1400356a3e3d50358d4e
Laust Rud Jacobsen <laust@valuestream.io>
2018-06-29 20:00:57 +0200

    EchoController: reduce duplication and add request_started_at to output

--- app/controllers/echo_controller.rb
+++ app/controllers/echo_controller.rb
@@ -1,9 +1,11 @@
 class EchoController < ApplicationController
 
   def index
+    queueing = RequestStore[:lograge_request_queueing]
     render json: {
-      request_queued_raw: RequestStore[:lograge_request_queueing].request_queued_raw,
-      queued_ms: RequestStore[:lograge_request_queueing].queued_ms
+      request_queued_raw: queueing.request_queued_raw,
+      request_started_at: queueing.request_started_at,
+      queued_ms: queueing.queued_ms,
     }
   end
 
