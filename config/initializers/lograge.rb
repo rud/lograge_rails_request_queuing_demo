4b76809d8ce350e0790fc26b477e1cec9701a939
Laust Rud Jacobsen <laust@valuestream.io>
2018-06-29 11:47:38 +0200

    lograge: log exceptions without verbose multi-line debug output

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -4,12 +4,21 @@ Rails.application.configure do
   # Keep emitting the verbose logging for easier debug
   config.lograge.keep_original_rails_log = !Rails.env.production?
 
-  config.lograge.custom_options = lambda do |_event|
+  config.lograge.custom_options = lambda do |event|
     custom_options = {}
 
     queued_ms = RequestStore[:lograge_request_queueing].queued_ms
     custom_options[:rq] = queued_ms.round(2) if queued_ms
 
+    if event.payload[:exception_object]
+      custom_options[:exception] = event.payload[:exception]
+      custom_options[:backtrace] = Array(event.payload[:exception_object].backtrace)
+    end
+
     custom_options
+  end
+
+  if Rails.env.production?
+    Rails.application.middleware.delete(ActionDispatch::DebugExceptions)
   end
 end
