60778c9a1c1381747ed7abf093e6dc92000520ab
Laust Rud Jacobsen <laust@valuestream.io>
2018-06-29 20:41:22 +0200

    echo#index: allow raising exceptions on demand
    For demo purposes

--- app/controllers/echo_controller.rb
+++ app/controllers/echo_controller.rb
@@ -1,7 +1,10 @@
 class EchoController < ApplicationController
 
   def index
+    raise ArgumentError, 'Insufficient mittens' if params[:demo_raise]
+
     queueing = RequestStore[:lograge_request_queueing]
+
     render json: {
       request_queued_raw: queueing.request_queued_raw,
       request_started_at: queueing.request_started_at,
